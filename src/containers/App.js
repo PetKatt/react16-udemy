0bbf975cdc9de78ca433000eaf7d1333e1d82261
Piotr Wiercinski <piotrwier@o2.pl>
2018-03-30 15:49:19 +0200

    sec7 branch: setState() used with a function

--- src/containers/App.js
+++ src/containers/App.js
@@ -15,7 +15,8 @@ class App extends PureComponent {
         { id:"dsa3125", name: "Manu", age: 29 },
         { id:"csa3124", name: "Stephanie", age: 26 }
       ],
-      showPersons: false
+      showPersons: false,
+      toggleClicked: 0
     };
     console.log("[App.js] Inside constructor", props); 
   }
@@ -76,9 +77,10 @@ class App extends PureComponent {
 
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
-    this.setState({
-      showPersons: !doesShow
-    });
+    this.setState((prevState, props) => ({
+      showPersons: !doesShow,
+      toggleClicked: prevState.toggleClicked + 1
+    }));
   }
 
   render() {
